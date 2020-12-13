89c7c996d592ba098ae59aa51ef9a1367b44c5c0
Htet Lin Maung <jsthtet96@gmail.com>
2020-12-13 09:15:32 +0630

    upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get("/video", (req, res) => {
